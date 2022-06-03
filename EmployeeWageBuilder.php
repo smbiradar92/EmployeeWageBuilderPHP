ff86b14d7e2dd48dbe8840b238ec8be74d460132
Smbiradar92 <smbiradar92@gmail.com>
2022-06-03 21:00:51 +0530

    Added Program to check Employee Daily Wage

--- EmployeeWageBuilder.php
+++ EmployeeWageBuilder.php
@@ -11,14 +11,22 @@ class EmployeeWage
     function AttendenceCheck()
     {
 
+        $wagePerHour = 20;
         $isPresent = 1;
-        $random = rand(0, 1);
+        $random = rand(0, 1);                   //random value to check attendence
+        $workingHour = 0;
 
-        if ($random == $isPresent) {
-            echo "Enployee is Present";
+        if ($random == $isPresent) {            //Condition to check Employee Attendence
+            echo "Employee is Present\n";
+            $workingHour = 8;
         } else {
-            echo "Employee is Absent";
+            echo "Employee is Absent\n";
+            $workingHour = 0;
         }
+
+        //calculation of EmployeeWage
+        $dailyEmployeeWage = $wagePerHour * $workingHour;       
+        echo "Employee daily wage is: " . $dailyEmployeeWage;
     }
 }
 EmployeeWage::welcomeMsg();
