4a859c57987b1e68718daae7969431dbc10f690b
Smbiradar92 <smbiradar92@gmail.com>
2022-06-03 19:42:21 +0530

    Program to check Employeee Attedence

--- EmployeeWageBuilder.php
+++ EmployeeWageBuilder.php
@@ -1,17 +1,26 @@
 <?php
 
-class EmployeeWage {
+class EmployeeWage
+{
 
-    static function welcomeMsg(){
-        echo "Welcome to Employee Wage builder";
+    static function welcomeMsg()
+    {
+        echo "Welcome to Employee Wage builder\n";
     }
 
+    function AttendenceCheck()
+    {
+
+        $isPresent = 1;
+        $random = rand(0, 1);
+
+        if ($random == $isPresent) {
+            echo "Enployee is Present";
+        } else {
+            echo "Employee is Absent";
+        }
+    }
 }
 EmployeeWage::welcomeMsg();
-
-
-
-
-
-
-?>
+$employee = new EmployeeWage();
+$employee->AttendenceCheck();
