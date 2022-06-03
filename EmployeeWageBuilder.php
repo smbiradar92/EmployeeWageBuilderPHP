dcccd8a0f78e28bf62262befbe974dae5c289921
Smbiradar92 <smbiradar92@gmail.com>
2022-06-03 22:12:15 +0530

    Program to calculate Monthly Employee wage

--- EmployeeWageBuilder.php
+++ EmployeeWageBuilder.php
@@ -14,25 +14,40 @@ class EmployeeWage
         $wagePerHour = 20;
         $isPresent = 1;
         $isPartTime = 2;
-        $random = rand(0, 2);                   //random value to check attendence
-        $workingHour = 0;
+        $workingHours = 0;
+        $totalWorkingHours = 0;
+        $workingDays = 0;
+        $maximumWorkingDays = 20;
 
-        if ($random == $isPresent) {            //Condition to check Employee Attendence
-            echo "Employee is Present\n";
-            $workingHour = 8;
-        }elseif($random == $isPartTime){
-            echo "Employee is Parttime\n";
-            $workingHour = 4;
-        }else{
-            echo "Employee is Absent\n";
-            $workingHour = 0;
+        while ($workingDays <= $maximumWorkingDays) {             // condition for calculating wages for a month
+            $random = rand(0, 2);                   //random value to check attendence
+            echo "Day " . $workingDays . "\n";
+            switch ($random) {                       //switch case condition
+                case 1:
+                    echo "Employee is Present\n";
+                    $workingHours = 8;
+                    break;
+                case 2:
+                    echo "Employee is PartTime\n";
+                    $workingHours = 4;
+                    break;
+                default:
+                    echo "Employee is Absent\n";
+                    $workingHours = 0;
+            }
+            //calculating total working hours    
+            $workingDays++;
+            $totalWorkingHours = $totalWorkingHours + $workingHours;            //Adding hours in loop
+
         }
-
         //calculation of EmployeeWage
-        $dailyEmployeeWage = $wagePerHour * $workingHour;       
-        echo "Employee daily wage is : $" . $dailyEmployeeWage;
+        echo "Total working Hours = " . $totalWorkingHours . "\n";
+        $monthlyWage = $totalWorkingHours * $wagePerHour;
+        echo "Employee total monthly wage is : $" . $monthlyWage . "\n";
     }
 }
 EmployeeWage::welcomeMsg();
 $employee = new EmployeeWage();             //creating class object
 $employee->AttendenceCheck();               //calling function
+
+?>
